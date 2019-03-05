a88cb62d1195701d97071b56ee257162b845d6ea
css9 <cjd2015@qq.com>
2019-03-05 15:31:11 +0800

    add function

--- module.php
+++ module.php
@@ -9,5 +9,19 @@
 defined('IN_IA') or exit('Access Denied');
 
 class Css9_idiom_guessModule extends WeModule {
-	
+	public function settingsDisplay($settings) {
+        global $_GPC, $_W;
+        if (checksubmit()) {
+            $cfg = array(
+                'layout' => $_GPC['layout'],
+                'exceptWord' => $_GPC['exceptWord'],
+	            'charTotal' => $_GPC['charTotal'],
+            );
+            if ($this->saveSettings($cfg)) {
+                message('保存成功', 'refresh');
+            }
+        }
+        load()->func('tpl');
+		include $this->template('setting');
+    }
 }
